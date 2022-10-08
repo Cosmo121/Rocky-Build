52550ae8f14c5b83f8c9a460f9a1edc0dad66598
Mike <michael.hartman0@gmail.com>
2022-10-08 16:28:23 -0700

    Create portainer.sh

---
+++ portainer.sh
@@ -0,0 +1,13 @@
+# Start Portainer Setup #
+
+# Create volume for the Portainer Server database
+docker volume create portainer_data
+
+# Install Portainer Server
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+# Check that it is running
+docker ps
+
+# log in
+# https://$ipaddress:9443
