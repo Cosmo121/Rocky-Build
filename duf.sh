0b9fabe44aeb6402f7783af60f98fa841f3ed66f
Mike <michael.hartman0@gmail.com>
2023-04-22 12:56:09 -0700

    Update duf.sh

--- duf.sh
+++ duf.sh
@@ -1,5 +1,8 @@
 # https://github.com/muesli/duf
 
+# if you don't have wget already
+yum install wget
+
 wget https://github.com/muesli/duf/releases/download/v0.8.1/duf_0.8.1_linux_amd64.rpm
 
 rpm -Uvh duf_0.8.1_linux_amd64.rpm
