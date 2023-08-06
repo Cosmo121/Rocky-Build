ce7655108c2a34ec344519fe4dac96a53a2e4eb1
Mike <michael.hartman0@gmail.com>
2023-08-05 23:05:06 -0700

    Update domain_join.sh

--- domain_join.sh
+++ domain_join.sh
@@ -6,3 +6,9 @@ nmcli general hostname $linuxexamplehostname
 
 # join realm
 realm join --user=domainjoinaccount domain.com -v
+
+# add priviledged group to login
+realm permit -g securitygroup@domain.com
+
+# edit sudo file
+visudo
